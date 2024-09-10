d6ad2774ba538a155a606951504e11bf9f5f9981
Mahendra kumar Yadav <77223238+mahi2120@users.noreply.github.com>
2024-09-10 11:31:32 +0530

    Add files via upload

---
+++ Basic Page.php
@@ -0,0 +1,180 @@
+<style>
+	h1 {
+		font-size: 30px;
+	}
+	
+	.table-boxed {
+		border: 1px solid #e5e5e5;
+		background-color: #fff;
+		box-shadow: 0 3px 10px #d7d7d7;
+	}
+	
+	.table> thead> tr> th {
+		border-bottom: 2px solid #53b50a;
+	}
+	
+	.table> tbody> tr> td,
+	.table> tbody> tr> th,
+	.table> tfoot> tr> td,
+	.table> tfoot> tr> th,
+	.table> thead> tr> td,
+	.table> thead> tr> th {
+		padding: 10px;
+		border-top: 1px dashed #d7d7d7;
+	}
+#testPaperStructure ul {
+	padding-left: 30px;
+}	
+#testPaperStructure ul li {
+	list-style: disc;
+}
+#testPaperStructure .title {font-size: 25px;margin: 10px 0px 0px 0px !important;}	
+.table td, #paperFormat th {
+	text-align: center;
+	vertical-align: middle;}
+	
+	
+.mainDivPatter>div {
+  background: #ffffff;
+  padding: 30px 22px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  border-radius: 6px;
+  border: 1px solid #E2E4FF;
+  position: relative;
+  box-shadow: 0 0 15px rgba(0, 122, 255, 0.12);
+  margin: 15px;
+}
+.mainDivPatter .icon, .mainDivPatter .content {
+	text-align: center;
+}	
+.mainDivPatter .content {
+	margin-top: 10px;
+}
+</style>
+<!-- ~~~ Hero Section ~~~ -->
+<section class="hero-section banner-overlay bg_img" data-img="<?php echo $this->config->item('templateassets');?>images/banner/banner-bg.jpg">
+	<div class="hero-thumb d-none d-lg-block">
+		<img src="<?php echo $this->config->item('templateassets');?>images/banner/hero.png" alt="banner">
+	</div>
+	<div class="custom-container">
+		<div class="hero-content">
+			<h1 class="uppercase cl-white">Pattern & Syllabus for Stage-2 exam</h1>
+			<ul class="breadcrumb cl-white p-0 m-0">
+				<li>
+					<a href="<?php echo site_url();?>">Home</a>
+				</li>
+				<li>
+					Pattern & Syllabus
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
+<!-- ~~~ Hero Section ~~~ -->
+
+
+<!-- ~~~ About Section ~~~ -->
+<section class="about-section pt-50 pb-50">
+	<div class="container">
+		<div class="row mainDivPatter">
+				<div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-12">
+					<div class="icon">
+						<a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_5_new.pdf" target="_blank" tabindex="0">
+							<img src="<?php echo $this->config->item('templateassets')?>images/syllabusPattern/08.png" style="width:100px;" alt="Matrix Olympiad 2022-23">
+						</a>
+					</div>
+					<div class="content">
+						<h6 class="title"><a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_5_new.pdf" target="_blank" tabindex="0">Class V</a></h6>
+						<span>Syllabus &amp; Pattern</span>
+					</div>
+					
+				</div>   
+				<div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-12">
+					<div class="icon">
+						<a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_6_new.pdf" target="_blank" tabindex="0">
+							<img src="<?php echo $this->config->item('templateassets')?>images/syllabusPattern/06.png" style="width:100px;" alt="Matrix Olympiad 2022-23">
+						</a>
+					</div>
+					<div class="content">
+						<h6 class="title"><a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_6_new.pdf" target="_blank" tabindex="0">Class VI</a></h6>
+						<span>Syllabus &amp; Pattern</span>
+					</div>
+					
+				</div>             
+				<div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-12">
+							<div class="icon">
+								<a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_7_new.pdf" target="_blank" tabindex="0">
+									<img src="<?php echo $this->config->item('templateassets')?>images/syllabusPattern/07.png" style="width:100px;" alt="Matrix Olympiad 2022-23">
+								</a>
+							</div>
+							<div class="content">
+								<h6 class="title"><a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_7_new.pdf" target="_blank" tabindex="0">Class VII</a></h6>
+								<span>Syllabus &amp; Pattern</span>
+							</div>
+							
+						</div>
+				<div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-12">
+							<div class="icon">
+								<a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_8_new.pdf" target="_blank" tabindex="0">
+									<img src="<?php echo $this->config->item('templateassets')?>images/syllabusPattern/08.png" style="width:100px;" alt="Matrix Olympiad 2022-23">
+								</a>
+							</div>
+							<div class="content">
+								<h6 class="title"><a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_8_new.pdf" target="_blank" tabindex="0">Class VIII</a></h6>
+								<span>Syllabus &amp; Pattern</span>
+							</div>
+							
+						</div>
+				<div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-12">
+							<div class="icon">
+								<a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_9_new.pdf" target="_blank" tabindex="0">
+									<img src="<?php echo $this->config->item('templateassets')?>images/syllabusPattern/09.png" style="width:100px;" alt="Matrix Olympiad 2022-23">
+								</a>
+							</div>
+							<div class="content">
+								<h6 class="title"><a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_9_new.pdf" target="_blank" tabindex="0">Class IX</a></h6>
+								<span>Syllabus &amp; Pattern</span>
+							</div>
+							
+						</div>
+				<div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-12">
+							<div class="icon">
+								<a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_10_new.pdf" target="_blank" tabindex="0">
+									<img src="<?php echo $this->config->item('templateassets')?>images/syllabusPattern/10.png" style="width:100px;" alt="Matrix Olympiad 2022-23">
+								</a>
+							</div>
+							<div class="content">
+								<h6 class="title"><a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_10_new.pdf" target="_blank" tabindex="0">Class X</a></h6>
+								<span>Syllabus &amp; Pattern</span>
+							</div>
+							
+						</div>
+				<div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-12">
+							<div class="icon">
+								<a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_11_new.pdf" target="_blank" tabindex="0">
+									<img src="<?php echo $this->config->item('templateassets')?>images/syllabusPattern/11.png" style="width:100px;" alt="Matrix Olympiad 2022-23">
+								</a>
+							</div>
+							<div class="content">
+								<h6 class="title"><a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_11_new.pdf" target="_blank" tabindex="0">Class XI</a></h6>
+								<span>Syllabus &amp; Pattern</span>
+							</div>
+							
+						</div>
+				<div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-12">
+							<div class="icon">
+								<a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_12_new.pdf" target="_blank" tabindex="0">
+									<img src="<?php echo $this->config->item('templateassets')?>images/syllabusPattern/12.png" style="width:100px;" alt="Matrix Olympiad 2022-23">
+								</a>
+							</div>
+							<div class="content">
+								<h6 class="title"><a href="<?php echo $this->config->item('templateassets')?>syllabus/syllabus_2024_12_new.pdf" target="_blank" tabindex="0">Class XII</a></h6>
+								<span>Syllabus &amp; Pattern</span>
+							</div>
+							
+						</div> 
+		</div>
+	</div>
+</section>
+<!-- ~~~ About Section ~~~ -->
